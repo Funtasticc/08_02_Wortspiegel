02d096cc8b7cdeff0ecc58dc7debd7ed425fa6e5
Steffen <steffen.moeller1993@gmx.net>
2016-12-01 09:04:41 +0100

    Update main.cpp

--- 08_02_Wortspiegel/main.cpp
+++ 08_02_Wortspiegel/main.cpp
@@ -4,52 +4,52 @@
 using namespace std;
 void writeOut(string myString, int position)
 {
-	bool foundPosition = false;
-	cout << myString << endl;
-	for (int i = 0; i < myString.length(); i++)
+	bool foundPosition = false;			// um auf Wahr zu springen, wenn Position gefunden wurde
+	cout << myString << endl;			// gibt den eingegebenen Text aus
+	for (int i = 0; i < myString.length(); i++)	// schleife soll bis länge des Textes gehen
 	{
 		if (position < 0) {}
 		else if (i == position)
 		{
-			cout << "*" << endl;
-			foundPosition = true;
+			cout << "*" << endl;		// Sternchen unter dem Text
+			foundPosition = true;		// solange die Postition gefunden werden kann
 		}
-		else if (!foundPosition) cout << " ";
-	}
+		else if (!foundPosition) cout << " "; // wenn über die Länge hinaus gearbeitet wird, erscheint kein
+	}											// Sternchen mehr
 }
-char getCommand()
+char getCommand()			// char Funktion, um Befehl entgegen zu nehmen
 {
 	char myChar;
-	cout << "Befehl (l: links, r : rechts, s : spiegeln, q : Ende) ?- ";
+	cout << "Befehl (l: links, r : rechts, s : spiegeln, q : Ende) ?- "; // Die Befehle zur Auswahl einblenden
 	cin >> myChar;
 	cout << endl;
-	return myChar;
+	return myChar; // char wird übergeben
 }
 int main()
 {
-	string myString;
-	int stringPosition = 0;
-	char myCommand;
+	string myString;		//String wird definiert
+	int stringPosition = 0;	//es wird von Position 0 ausgegangen
+	char myCommand;			//myCommand wird definiert
 	cout << "Bitte geben Sie den Text ein: ? ";
 	getline(cin, myString);
 	do
 	{
 		writeOut(myString, stringPosition);
 		myCommand = getCommand();
-		if (myCommand == 'l')
+		if (myCommand == 'l')								// wenn l eingegeben wird
 		{
-			if (stringPosition > 0) stringPosition -= 1;
+			if (stringPosition > 0) stringPosition -= 1;	// Sternchen nach links, wenn größer als 1
 		}
-		else if (myCommand == 'r')
+		else if (myCommand == 'r')							// wenn r eingegeben wird
 		{
-			if (stringPosition < myString.length())
+			if (stringPosition < myString.length())			// funktioniert, solange es innerhalb der Länge ist
 			{
-				stringPosition += 1;
+				stringPosition += 1;						// Sternchen nach rechts
 			}
 		}
-		else if (myCommand == 's') 
-				wortspiegel(myString, stringPosition);
-	} while (myCommand != 'q');
+		else if (myCommand == 's')							// Funktion wortspiegel wird benutzt
+			wortspiegel(myString, stringPosition);
+	} while (myCommand != 'q');								// Schleife endet, wenn q eingegeben wird
 
 	system("pause");
 	return 0;
